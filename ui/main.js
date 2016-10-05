da8f1cfe60f35a09346753904c07990e1440f221
neerajparmaar <neeraj.parmaar@gmail.com>
2016-10-05 13:14:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,7 @@
-console.log('Loaded!');
-var element=document.getElementById("maintext");
-element.innerHTML="Update";
-var img=document.getElementById("dino");
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-    var interval=setInterval(moveRight,50);
-    
+var counter=0;
+var button=document.getElementById("counter");
+button.onclick=function(){
+    counter=counter+1;
+    var span=document.getElementById("count");
+    span.innerHTML=counter.toString();
 }
