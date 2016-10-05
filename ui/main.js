52b30f002ad7ee6d6785557c06b077042dfabbe9
neerajparmaar <neeraj.parmaar@gmail.com>
2016-10-05 11:45:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById("maintext");
 element.innerHTML="Update";
+var img=document.getElementById("dino");
+img.onclick=function(){
+    img.style.marginLeft="100px";
+}
