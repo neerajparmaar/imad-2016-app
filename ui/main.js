f59518968ad53831fb11da99650878d236ace7d8
neerajparmaar <neeraj.parmaar@gmail.com>
2016-10-06 15:49:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,3 +16,14 @@ button.onclick=function(){
     request.open('GET','http://neerajparmaar.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+var nameInput=document.getElementById("name");
+var name=nameInput.value;
+var submit=document.getElementById("submit_btn");
+submit.onclick=function(){
+     var name=['name1','name2','name2','name4'];
+     var list="";
+     for(var i=0;i<names.length;i++)
+     { list+="<li>"+names[i]+"</li>"; }
+     var ul=document.getElementById("namelist");
+     ul.innerHTML=list;
+};
