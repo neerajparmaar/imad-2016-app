c1f3c9c52b47f54e4b8f0343756bcb91ce1f39ca
neerajparmaar <neeraj.parmaar@gmail.com>
2016-10-06 16:16:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,29 +1,23 @@
-var counter=0;
-var button=document.getElementById("counter");
-button.onclick=function(){
-    var request=new XMLHttpRequest();
-    request.onreadystatechange=function(){
-        if(request.readyState===XMLHttpRequest.DONE){
-            if(request.status===200){
-                var counter=request.responseText;
-                var span=document.getElementById("count");
-                span.innerHTML=counter.toString();
-            } 
-        }
-          
-    };
-
-    request.open('GET','http://neerajparmaar.imad.hasura-app.io/counter',true);
-    request.send(null);
-};
 var nameInput=document.getElementById("name");
 var name=nameInput.value;
 var submit=document.getElementById("submit_btn");
 submit.onclick=function(){
-     var names=['name1','name2','name2','name4'];
-     var list="";
-     for(var i=0;i<names.length;i++)
-     { list+="<li>"+names[i]+"</li>"; }
-     var ul=document.getElementById("namelist");
-     ul.innerHTML=list;
+    var request=new XMLHttpRequest();
+      request.onreadystatechange=function(){
+         if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+               var names=request.responseText;
+               names=JSON.parse(names);
+               var list="";
+                for(var i=0;i<names.length;i++)
+                   { list+="<li>"+names[i]+"</li>"; }
+                var ul=document.getElementById("namelist");
+                ul.innerHTML=list;
+            } 
+          }
+          
+    };
+
+    request.open('GET','http://neerajparmaar.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
 };
